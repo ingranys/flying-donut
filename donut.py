Enhance code - Fix projection formula - Speed up calculation - Add reset console - Add preview mode

donut.py
@@ -39,63 +39,81 @@ import curses
 import numpy as np
 
 from utils.geom import base, donut, rotations, rotate, projection,shades
-from utils.render import points, vectors, colors, animate3d
+from utils.render import points, vectors, colors, image, animate3d
 
 R1 = 1
 R2 = 2
 
-n_theta = 50
-n_phi = 500
-
-size = 12
+n_theta = 80
+n_phi = 200
 n_points = n_phi*n_theta
-spotlight = np.array([-1,2,7])
-char = [" ", ". ", ": ", "; ", "= ", "! ", "* ", "# ", "$ ", "@ "]
-char = [" ", ". ", ": ", "; ", "+ ", "? ", "% ", "S ", "# ", "@ "]
 
+start_angleA = 0.5
+start_angleB = -0.5
+
+spotlight = np.array([1,1,8])
+
+size = 16
+n_frames = 500
+speed = 1/100
+
+preview = False
+
+
+char = [" ", ". ", ": ", "; ", "= ", "! ", "* ", "# ", "$ ", "@ "]
+char = [" ", ", ", "~  ", "; ", "= ", "! ", "* ", "# ", "$ ", "@ "]
+#char = [" ", ". ", ": ", "; ", "+ ", "? ", "% ", "S ", "# ", "@ "]
 
 X,Y,Z = base()
-M_donut,V_normals = donut(R1*X,R2*X,Z,n_theta,Y,n_phi)
-#points(M_donut,-90,135)
+M_donut,V_normals, M_cirlces = donut(R1*X,R2*X,Z,n_theta,Y,n_phi)
+#points(M_cirlces,-90,135,10)
+#points(M_donut,-90,135,10)
 #vectors(M_donut,V_normals,-90,135,5)
 
-M_rotated_donut = rotate(M_donut, rotations(X,1,Z,1))
-V_rotated_normals = rotate(V_normals, rotations(X,1,Z,1))
-#points(M_rotated_donut,-90,135)
+M_rotated_donut = rotate(M_donut, rotations(X,start_angleA,Z,start_angleB))
+V_rotated_normals = rotate(V_normals, rotations(X,start_angleA,Z,start_angleB))
+rotations = rotations(X,7*speed,Z,3*speed)
+#points(M_rotated_donut,-90,135,10)
 #vectors(M_rotated_donut,V_rotated_normals,-90,135,5)
-#animate3d(M_rotated_donut,rotations(X,(2*np.pi)*7/100,Y,(2*np.pi)*3/100),rotate,5,50,-90,100,5)
+if preview:   
+    initial_shades, _ = shades(M_donut,V_normals,spotlight)
+    rotated_shades, _ = shades(M_rotated_donut,V_rotated_normals,spotlight)
+    colors(M_donut,initial_shades,-90,135,10)
+    colors(M_rotated_donut,rotated_shades,-90,135,10)
+    animate3d(M_rotated_donut,rotations,rotate,5,10,-90,135,10)
 
-#initial_shades = shades(M_donut,V_normals,spotlight)
-rotated_shades = shades(M_rotated_donut,V_rotated_normals,spotlight)
-#colors(M_donut,initial_shades,-90,135,10)
-#colors(M_rotated_donut,rotated_shades,-90,135,10)
 
 scr = curses.initscr()
 scr.clear()
 scr.refresh()
 
-rotations = rotations(X,(2*np.pi)*7/100,Y,(2*np.pi)*3/100)
-for k in range(99):
+#start = time.time()
+for k in range(n_frames):
     M_rotated_donut = rotate(M_rotated_donut, rotations)
     V_rotated_normals = rotate(V_rotated_normals, rotations)
-    rotated_shades = shades(M_rotated_donut,V_rotated_normals,spotlight)
+    rotated_shades, light_indexes = shades(M_rotated_donut,V_rotated_normals,spotlight)
 
-    M_pixels = np.zeros((2*size,2*size))
-    M_resized_donut = (size+np.floor((2*size/7)*M_rotated_donut[:,:2])).astype('int')
-    for i in range(n_points):
-        x, y = M_resized_donut[i]
-        M_pixels[y,x] = np.maximum(rotated_shades[i],M_pixels[y,x])
-    M_ascii_pixels = [[char[int(np.floor(10*M_pixels[i,j]))] for i in range(2*size)] for j in range(2*size)]
+    M_pixels = np.zeros((2*size,4*size))
+    x_donut = (2*size+np.floor((4*size/7)*M_rotated_donut[:,0])).astype('int')
+    y_donut = (size-np.floor((2*size/7)*M_rotated_donut[:,1])).astype('int')
+    for idx in light_indexes:
+        x = x_donut[idx]
+        y = y_donut[idx]
+        M_pixels[y,x] = np.maximum(rotated_shades[idx],M_pixels[y,x])
+    M_ascii_pixels = [[char[int(np.floor(10*M_pixels[i,j]))] for j in range(4*size)] for i in range(2*size)]
+
+    #image(M_pixels)
 
     scr.addstr(0, 0, str(k) )
     for i in range(2*size):
-        for j in range(2*size):
-            scr.addstr(i, 2*j, M_ascii_pixels[i][j])
-    scr.addstr(0, 0, str(k) )
+        for j in range(4*size):
+            scr.addstr(i, j, M_ascii_pixels[i][j])
+    msg = '{0}/{1}'.format(k+1,n_frames)
+    scr.addstr(0, 0, msg )
     scr.refresh()
-scr.clear()
-scr.refresh()
 
+curses.endwin()
+#print("{0} sec".format(time.time()-start))
 
 """
 if __name__ == '__main__':

utils/geom.py
@@ -19,7 +19,7 @@ def donut(V_R1,V_R2,V_theta,n_theta,V_phi,n_phi):
 
     M_circle = np.array([R.from_rotvec(theta_angle*V_theta).apply(V_R1) for theta_angle in theta_angles])
     M_translated_circle = M_circle + V_R2
-    #points(np.vstack((M_circle,M_translated_circle)),-90,135,5)
+    M_circles = np.vstack((M_circle,M_translated_circle))
         
     phi_rotations = [R.from_rotvec(phi_angle*V_phi) for phi_angle in phi_angles]
     M_rotated_circles = np.array([phi_rotation.apply(M_translated_circle) for phi_rotation in phi_rotations])
@@ -27,7 +27,7 @@ def donut(V_R1,V_R2,V_theta,n_theta,V_phi,n_phi):
     M_donut = M_rotated_circles.reshape(n_points,3)
     V_normals = np.array([phi_rotation.apply(M_circle) for phi_rotation in phi_rotations]).reshape(n_points,3)
 
-    return M_donut,V_normals
+    return M_donut,V_normals,M_circles
 
 
 
@@ -62,4 +62,6 @@ def projection(V1,V2):
 
 def shades(M,N,s):
     L = s - M
-    return projection(L,N)
+    lambert = projection(L,N)
+    light_indexes = np.argwhere(lambert>0).T
+    return lambert,light_indexes

utils/render.py
@@ -69,7 +69,7 @@ def image(pixels):
     plt.show()
 
 def update3d(i, rotations, function, scat):
-    donut = scat.get_array()
+    donut = np.array(scat._offsets3d).T
     rotated_donut = function(donut,rotations)
     scat._offsets3d = (rotated_donut[:,0],rotated_donut[:,1],rotated_donut[:,2])
     return scat