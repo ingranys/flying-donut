242fa457b0c79d6df1a20a17419348b377542d0e
ingranys <ingranys@protonmail.com>
2021-01-16 14:50:52 +0100

    Add comments and docstrings to console.py and render.py

--- utils/console.py
+++ utils/console.py
@@ -1,39 +1,101 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" ASCII rendering module for flying donut.
+
+For more information, see README.
+
+For usage, run <python3 donut.py>.
+
+Project can be found here <https://github.com/ingranys/flying-donut>.
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+__author__ = "ingranys"
+__contact__ = "ingranys@protonmail.com"
+__copyright__ = "Copyright 2021, Mustapha Gaies, Toulouse (France)"
+__date__ = "2021/01/16"
+__deprecated__ = False
+__email__ =  "ingranys@protonmail.com"
+__license__ = "GPLv3"
+__maintainer__ = None
+__status__ = "Production"
+__version__ = "0.1.0"
+
+
 import sys
 import time
 import curses
 import numpy
 
 
-def warning(mode='<mode',duration=10):
+def warning(mode='<mode>',duration=10):
+    """Print user warning.
+
+    Args:
+        mode (str, optional): Mode (can PREVIEW or DEBUG). Defaults to '<mode>'.
+        duration (int, optional): Message duration in seconds. Defaults to 10.
+    """
     print('WARNING!')
     print('{0} mode is enabled with a large number of points.'.format(mode))
     print('Be aware of possible lag while rendering.')
     for remaining in range(duration, -1, -1):
         print("Continue in {:2d} seconds...".format(remaining),end="\r")
         time.sleep(1)
+    # Use ASCII escape sequence
     #https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
     print("\033[2K",end="\r")
     print("Moving on to the rendering.")
 
 
 def screen(n_pixels):
+    """Initialize ascii screen.
+
+    Args:
+        n_pixels (int): Screen size in pixels (each pixel is ASCII character).
+
+    Returns:
+        scr (screen): Initialized screen.
+        frame_height (int) : ASCII frame height. 
+        frame_width (int) : ASCII frame width (ASCII image aspect ratio is 2:1).
+    """
+    # Initialize screen
     scr = curses.initscr()
 
+    # Get the maximum possible size for the current console
     screen_height,screen_width= scr.getmaxyx()
+    # Get the maximum possible size for the ascii screen within the console
+    # IMPORTANT! It takes twice as much columns than lines to draw ascii art
     frame_size = min(screen_height,numpy.floor(screen_width/2).astype(int))
     if n_pixels<=0:
+        # Auto-scale
         frame_height = frame_size
         frame_width = 2*frame_size
     elif n_pixels<=frame_size:
+        # Manual scale
         frame_height = n_pixels
         frame_width = 2*n_pixels
     else :
+        # Contraints can't be applied
+        # Go back to previous console display and print error message
         curses.endwin()
         print('ERROR!')
         print('Image size exceeds console size.')
         print('Please decrease pixels number or widen console.')      
         sys.exit()         
 
+    # Clear screen to finish initialization
     scr.clear()
     scr.refresh()
 
@@ -41,25 +103,49 @@ def screen(n_pixels):
 
 
 def reset():
+    """Go back to previous console display
+    """
     curses.endwin()
 
 
 def asciis(pixels,char):
+    """Convert 2D grayscale image to ASCII characters.
+
+    Args:
+        pixels (array(float)): 2D array representing graysclale image.
+        char (array(str)): List of ASCII characters.
+
+    Returns:
+        ascii_characters (array(str)): 
+    """
     n_rows, n_columns = pixels.shape
     n_char = len(char)
+    # Map intensity values to characters
     ascii_characters =   [
                             [char[int(numpy.floor(n_char*pixels[i,j]))] 
                             for j in range(n_columns)] 
                         for i in range(n_rows)]
+                          
     return ascii_characters
 
 
 def render(ascii_characters,screen,current_frame,n_frames):
+    """Print ASCII characters to console.
+
+    Args:
+        ascii_characters (array[str]): 2D array containing ASCII characters.
+        screen (screen): ASCII screen. 
+        current_frame (int): Current frame number.
+        n_frames (int): Total number of frames.
+    """
+    # Get frame shape
     n_rows = len(ascii_characters)
     n_columns = len(ascii_characters[0])
+    # Print ASCII characters one by one
     for i in range(n_rows):
         for j in range(n_columns):
             screen.addstr(i, j, ascii_characters[i][j])
     msg = '{0}/{1}'.format(current_frame+1,n_frames)
+    # Refresh screen
     screen.addstr(0, 0, msg )
     screen.refresh()
--- utils/render.py
+++ utils/render.py
@@ -1,4 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" 3D and 2D rendering module for flying donut.
+
+For more information, see README.
+
+For usage, run <python3 donut.py>.
+
+Project can be found here <https://github.com/ingranys/flying-donut>.
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+__author__ = "ingranys"
+__contact__ = "ingranys@protonmail.com"
+__copyright__ = "Copyright 2021, Mustapha Gaies, Toulouse (France)"
+__date__ = "2021/01/16"
+__deprecated__ = False
+__email__ =  "ingranys@protonmail.com"
+__license__ = "GPLv3"
+__maintainer__ = None
+__status__ = "Production"
+__version__ = "0.1.0"
+
+
 import warnings
+# Ignore MatplotlibDeprecationWarning while importing matplotlib.animation
 warnings.filterwarnings("ignore",category=UserWarning)
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,6 +43,15 @@ import matplotlib.animation as animation
 
 
 def points(M,azimut=None,elevation=None,size=10,title='points'):
+    """Plot points in 3D. 
+
+    Args:
+        M (array([float]): Set of points (must be shape (n,3)).
+        azimut (float, optional): Azimuth angle for camera position (angle between x and p). Defaults to None.
+        elevation (float, optional): Elevation angle for camera position (angle between p and r). Defaults to None.
+        size (int, optional): Expected size of the object. Defaults to 10.
+        title (str, optional): Title for the figure window. Defaults to 'points'.
+    """
     fig = plt.figure(title.upper())
     ax = fig.add_subplot(projection='3d')
     ax.scatter(M[:,0],M[:,1],M[:,2])
@@ -25,6 +71,17 @@ def points(M,azimut=None,elevation=None,size=10,title='points'):
 
 
 def vectors(M,V,azimut=None,elevation=None,size=10,length=None,title='vectors'):
+    """Plot vectors in 3D.
+
+    Args:
+        M (array([float]): Set of vector locations (must be shape (n,3)).
+        V ([type]): Set of vectors (must be shape (n,3)).
+        azimut (float, optional): Azimuth angle for camera position (angle between x and p). Defaults to None.
+        elevation (float, optional): Elevation angle for camera position (angle between p and r). Defaults to None.
+        size (int, optional): Expected size of the object. Defaults to 10.
+        length ([type], optional): Length of the arrows. Defaults to None.
+        title (str, optional): Title for the figure window. Defaults to 'vectors'.
+    """
     fig = plt.figure(title.upper())
     ax = fig.add_subplot(projection='3d')
     ax.quiver(M[:,0],M[:,1],M[:,2],V[:,0],V[:,1],V[:,2])
@@ -46,6 +103,17 @@ def vectors(M,V,azimut=None,elevation=None,size=10,length=None,title='vectors'):
 
 
 def colors(M,colors,azimut=None,elevation=None,size=10,title='colors'):
+    """Plot points in 3D with colors.
+    Illumination intensities (grayscale) are automatically shifted to colors.
+
+    Args:
+        M (array([float]): Set of points (must be shape (n,3)).
+        colors (array[float]): Point colors (must be shape (n,)).
+        azimut (float, optional): Azimuth angle for camera position (angle between x and p). Defaults to None.
+        elevation (float, optional): Elevation angle for camera position (angle between p and r). Defaults to None.
+        size (int, optional): Expected size of the object. Defaults to 10.
+        title (str, optional): Title for the figure window. Defaults to 'colors'.
+    """
     fig = plt.figure(title.upper())
     ax = fig.add_subplot(projection='3d')
     ax.scatter(M[:,0],M[:,1],M[:,2],c=colors)
@@ -65,6 +133,11 @@ def colors(M,colors,azimut=None,elevation=None,size=10,title='colors'):
 
 
 def image(pixels):
+    """Display 2D grayscale image and change aspect ratio from 2:1 to 1:1.
+
+    Args:
+        pixels (array[float]): Pixel values (must shape (x,y)).
+    """
     y, x = pixels.shape
     plt.imshow(pixels,interpolation='none',cmap='gray',extent=[0,x,0,y],aspect=2)
 
@@ -72,6 +145,17 @@ def image(pixels):
 
 
 def update3d(i, rotations, function, scat):
+    """Update scene based on given rotations.
+
+    Args:
+        i (int): Iteration number. Not used.
+        rotations (array[rotation]): 3D rotations to be applied (must contain 1 more rotations).
+        function (function): Input function implementing rotation.
+        scat (array[float]): Scene information containing points in 3D.
+
+    Returns:
+        scat (array[float]): Updated scene.
+    """
     donut = np.array(scat._offsets3d).T
     rotated_donut = function(donut,rotations)
     scat._offsets3d = (rotated_donut[:,0],rotated_donut[:,1],rotated_donut[:,2])
@@ -80,6 +164,18 @@ def update3d(i, rotations, function, scat):
 
 
 def animate3d(donut,rotations,function,n_frames,azimut=None,elevation=None,size=10,title='animation'):
+    """Play 3D animation of the object moving according to rotations.
+
+    Args:
+        donut (array[float]): Set of points representing the object in 3D (shape must be (n,3))
+        rotations (array[rotation]): 3D rotations to be applied (must contain 1 more rotations).
+        function (function): Input function implementing rotation.
+        n_frames (int): Number of frames.
+        azimut (float, optional): Azimuth angle for camera position (angle between x and p). Defaults to None.
+        elevation (float, optional): Elevation angle for camera position (angle between p and r). Defaults to None.
+        size (int, optional): Expected size of the object. Defaults to 10.
+        title (str, optional): Title for the figure window. Defaults to 'animation'.
+    """
     fig = plt.figure(title.upper())
     ax = fig.gca(projection='3d')
     scat = ax.scatter(donut[:,0], donut[:,1], donut[:,2])
